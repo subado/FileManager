a23381ff761cc5b65e92241f51b9836cece6db0f
Subado <redcatgamer1980@gmail.com>
2023-01-21 23:38:07 +0400

    Init project

---
+++ include/FileManager.hpp
@@ -0,0 +1,17 @@
+#ifndef FILE_MANAGER_HPP
+#define FILE_MANAGER_HPP
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class FileManager
+{
+public:
+
+	FileManager();
+
+	~FileManager() = default;
+};
+
+#endif // FILE_MANAGER_HPP
